5a42097999fbcbf87dd809b02faf3d6e9b73f1f3
Andranik Agamalian <128445370+letscode94@users.noreply.github.com>
2024-08-05 22:46:16 -0700

    updated typescript within builtCacheROwDataParser.tsx file

--- ui/src/prune/modules/builtCascheRowDataParser.tsx
+++ ui/src/prune/modules/builtCascheRowDataParser.tsx
@@ -2,15 +2,15 @@
 
 
 
-export function BuiltCascheRowDataParser(output:any) {
+export function BuiltCascheRowDataParser(output:string) {
   
   const lines = output.split('\n');
-  const entriesFiltered:any = []; //array of arrays
-  const parsedBuiltCascheArray:any = []; //[{id: 'id', size: 'size', CreatedSince: '', reclaimable: false}]
+  const entriesFiltered: string[][] = []; //array of arrays
+  const parsedBuiltCascheArray: Array<{[key:string]: string}> = []; //[{id: 'id', size: 'size', CreatedSince: '', reclaimable: false}]
 
   // console.log("All Lines: \n",lines)
 
-  let totalLine:any= [];
+  let totalLine:string[]= [];
 
   /*we want to exclude the last element of the lines array because it contains information we are not utilizing.
   it also contains the key reclaimable with a different value then we expect causing an issue with the slice
@@ -18,6 +18,7 @@ export function BuiltCascheRowDataParser(output:any) {
 
   for(let i = 0; i < lines.length-1; i++){
     const ele = lines[i];
+    // console.log('ele', ele, typeof ele)
     /*each built-casche is seperated by an empty string due to the --verbose flag... 
     (Uncomment console.log("All Lines: \n",lines) on line 11 for representation)
     so everytime we get an empty string we know to push the totalLine array into the entriesFiltered array. 
@@ -29,14 +30,18 @@ export function BuiltCascheRowDataParser(output:any) {
         /*else we are still iterating through the elements of the totalLine array representing the current built casche. 
         So we filter through the totalLine array to make sure we only get the elements that we want. 
         Each element within the totalLine is a string. */
-        const filteredElementsInTotalLine = totalLine.filter((el:any)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g))
+        const filteredElementsInTotalLine:Array<string> = totalLine.filter((el:string)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g))
         
+        // console.log('filteredElementsInTotalLine', filteredElementsInTotalLine)
+
         //we want to make sure that the storage size of each built-casche is in MB. Without this algo below
         //we will have sizes of bytes, kilobytes as well.. 
-        filteredElementsInTotalLine.forEach((element:any)=>{
+        filteredElementsInTotalLine.forEach((element:string)=>{
           if(element.match(/MB/g)) {
             // console.log('element in filteredElementsInTotalLine -->', filteredElementsInTotalLine) 
-            entriesFiltered.push(totalLine.filter((el:any)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g)));
+            // console.log('totalLine', totalLine)
+            // console.log('totalLine.filter((el:any)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g)', totalLine.filter((el:any)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g)))
+            entriesFiltered.push(totalLine.filter((el:string)=> el.match(/ID/g) || el.match(/Size/g) || el.match(/Last/g) || el.match(/Reclaimable/g)));
           }
         })
         //we reset the totalLine into an empty array so we can fill it again prior to pushing into enteriesFiltered.
@@ -48,8 +53,11 @@ export function BuiltCascheRowDataParser(output:any) {
   We utilize the forEach method and the logic below to convert each array into an object with the data we need to represent
   within the dataGrid Rows. 
   */
-  entriesFiltered.forEach((element:any)=> {
-     const parsedObj:any = {ID: 'id', Size: 'size', CreatedSince: '', Reclaimable: 'False'};
+
+  // console.log('entriesFiltered', entriesFiltered)
+  entriesFiltered.forEach((element: Array<string>)=> {
+    type ParsedObjType = {ID:string, Size:string, CreatedSince:string, Reclaimable:string};
+    const parsedObj: ParsedObjType = {ID: 'id', Size: 'size', CreatedSince: '', Reclaimable: 'False'};
 
      parsedObj.ID = element[0].slice(5, element[0].length);
      parsedObj.Size = element[2].slice(7, element[2].length);
