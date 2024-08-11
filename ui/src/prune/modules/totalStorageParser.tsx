205f235d01f8c978d81e03d9dd05241ffc0dd57e
Andranik Agamalian <128445370+letscode94@users.noreply.github.com>
2024-08-10 17:24:54 -0700

    established totalStorageParser test file and added tests

--- ui/src/prune/modules/totalStorageParser.tsx
+++ ui/src/prune/modules/totalStorageParser.tsx
@@ -4,6 +4,9 @@ export function totalStorageParser(output:string):number {
 
     const lines = output.split('\n');
 
+    // console.log('output', output)
+    // console.log('lines', lines)
+    
     // console.log("All Lines -> totalStorageParser: \n",lines)
     let totalLine:string[]= [];
     
@@ -13,13 +16,21 @@ export function totalStorageParser(output:string):number {
       }
     })
 
+    // const output1 = `5.949GB`;
     // console.log("Line with Total:\n",totalLine[0].slice(8, totalLine[0].length));
+    //  console.log('test', output1.slice(0, output1.length).match(/GB/g));
+    //  console.log('real', totalLine[0].slice(8, totalLine[0].length).match(/GB/g))
+    //  console.log('test2', Number(output1.slice(0, output1.length-2)))
+    //  console.log('real2', Number(totalLine[0].slice(8, totalLine[0].length-2)));
   
     const builtCascheisGB =  totalLine[0].slice(8, totalLine[0].length).match(/GB/g);
     const convertBuiltCascheToNumber = Number(totalLine[0].slice(8, totalLine[0].length-2));
+
+    // console.log('builtCascheisGB', builtCascheisGB)
+    // console.log('convertBuiltCascheToNumber', convertBuiltCascheToNumber)
   
     if(builtCascheisGB) {
-      return convertBuiltCascheToNumber  * 1000;
+      return convertBuiltCascheToNumber * 1000;
     } else {
       return convertBuiltCascheToNumber;
     }
