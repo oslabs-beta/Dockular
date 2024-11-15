a4467c05cbbeecd4ee4465c7b36c6aea35ccaca2
Andranik Agamalian <128445370+letscode94@users.noreply.github.com>
2024-11-14 20:59:47 -0800

    refactored authenticator and added/configured sign out button

--- ui/src/advancedFeatures/authProvider.tsx
+++ ui/src/advancedFeatures/authProvider.tsx
@@ -3,8 +3,8 @@ import { Box } from "@mui/system";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-
- 
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 Amplify.configure({
     Auth: {
@@ -15,15 +15,49 @@ Amplify.configure({
     }
 })
 
+const formFields:any = {
+    signup: {
+        username: { 
+            order:1,
+            placeholder: "Choose a username",
+            label: "Username",
+            inputProps: {required: true}
+        },
+        email: { 
+            order:2,
+            placeholder: "Enter your email address",
+            label: "Email",
+            inputProps: {type:'email', required: true}
+        },
+        password: { 
+            order:3,
+            placeholder: "Enter your password",
+            label: "Password",
+            inputProps: {type:'password', required: true}
+        },
+        confirm_password: { 
+            order:4,
+            placeholder: "Confirm your password",
+            label: "Confirm Password",
+            inputProps: {type:'password', required: true}
+        },
+    },
+}
+
 const AuthProvider = ({children}:any) => {
-    return (<Box>
-        <Authenticator>{({user})=> 
-            user ? (
-                <Box>{children}</Box>
-            ) : 
-            <Box> Please Sign in Below:</Box>
-        }</Authenticator>
-    </Box>
+    return ( 
+
+        <Authenticator signUpAttributes={['email']}>{
+            // ({user})=> user ? (<Box>{children}</Box>) : <Box> Please Sign in Below:</Box>
+            ({ signOut, user }) => (
+                <Box>
+                  <Typography component="h1">{user?.username}</Typography>
+                  <Button onClick={signOut}>Sign out</Button>
+                </Box>
+              )}
+
+        </Authenticator>
+   
     )
 }
 
